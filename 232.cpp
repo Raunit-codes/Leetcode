9ce77bd3b4b164cb3eaf396be0dd2e7e6b37173c
Raunit Bakshi <raunitbakshi@gmail.com>
2026-09-13 20:25:23 +0530

    Implement MyQueue class using two stacks

---
+++ 232.cpp
@@ -0,0 +1,37 @@
+class MyQueue {
+private:
+    stack<int> in;
+    stack<int> out;
+
+public:
+    MyQueue() {}
+
+    void push(int x) { in.push(x); }
+
+    int pop() {
+
+        if (out.empty()) {
+            while (!in.empty()) {
+                out.push(in.top());
+                in.pop();
+            }
+        }
+        int value = out.top();
+        out.pop();
+        return value;
+    }
+
+    int peek() {
+
+        if (out.empty()) {
+            while (!in.empty()) {
+                out.push(in.top());
+                in.pop();
+            }
+        }
+
+        return out.top();
+    }
+
+    bool empty() { return in.empty() && out.empty(); }
+};
