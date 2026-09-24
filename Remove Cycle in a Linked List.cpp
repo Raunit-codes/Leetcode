566d6bd92f6a6649606515586d8aa80c2e85ff4c
Raunit Bakshi <raunitbakshi@gmail.com>
2026-09-24 09:53:31 +0530

    Correct cycle end detection in linked list
    Fix the logic to find the end of the cycle using the fast pointer.

---
+++ Remove Cycle in a Linked List.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    void removeCycle(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) {
+            return;
+        }
+
+        // Step 1: Detect cycle
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        bool hasCycle = false;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast) {
+                hasCycle = true;
+                break;
+            }
+        }
+
+        // No cycle
+        if (!hasCycle) {
+            return;
+        }
+
+        // Step 2: Find beginning of cycle
+        slow = head;
+
+        while (slow != fast) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+
+        // slow/fast = beginning of cycle
+
+        // Step 3: Find last node of cycle
+        while (fast->next != slow) {
+            fast = fast->next;
+        }
+
+        fast->next = nullptr;
+
+        // Break cycle
+        cycleEnd->next = nullptr;
+    }
+};
